ea5a6316c97dbf8e3c2a53951fb2b46a5224149e
razseed <christianmcrae@hotmail.com>
2022-03-11 15:04:35 +0000

    implimented navbar, debug potential typos as it dosent work, then set about fixing layout and colors of mainpage again. codepen.io/Homunculus84/pen/gOOqmvY for the source code to Simone Navbar

--- main.js
+++ main.js
@@ -1,11 +1,19 @@
-const menuBtn = document.querySelector('.menu-btn');
-let menuOpen = false;
-menuBtn.addEventListener('click', () => {
-    if(!menuOpen) {
-        menuBtn.classList.add('open');
-        menuOpen = true;
-    } else {
-        menuBtn.classList.remove('open');
-        menuOpen = false;
-    }
+//const menuBtn = document.querySelector('.menu-btn');
+//let menuOpen = false;
+//menuBtn.addEventListener('click', () => {
+    //if(!menuOpen) {
+   //     menuBtn.classList.add('open');
+   //     menuOpen = true;
+   // } else {
+ //       menuBtn.classList.remove('open');
+//        menuOpen = false;
+//    }
+//}); Iteration 1 of Navbar 
+
+//New Navbar by Simone @Homunculus84
+
+const navToggle = document.querySelector('.nav__toggle');
+
+navToggle.addEventListener('click',() => {
+    document.body.classList.toggle('nav-open');
 });    
